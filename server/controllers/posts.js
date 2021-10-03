e41198f6762f764e71a5b45b6b07a46203f39849
Sundara Ram Matta <msram0210@gmail.com>
2021-10-03 12:25:21 -0400

    adding the createPost call

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,5 +2,9 @@
 // Handle logic here instead under routes
 
 export const getPosts = (req,res) => {
-  res.send("THIS WORKS!");
+  res.send("THIS WORKS NOW!");
+};
+
+export const createPosts = (req,res) => {
+  res.send("POST CREATION");
 };
