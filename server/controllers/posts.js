765e4901e37e318899e9af018b4e7591df302ca2
Sundara Ram Matta <msram0210@gmail.com>
2021-10-03 12:32:48 -0400

    imporving the get call by addingthe try and catch block

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,16 @@
 // Handlers for our route /posts
 // Handle logic here instead under routes
 
-export const getPosts = (req,res) => {
-  res.send("THIS WORKS NOW!");
+import PostMsg from '../models/postMsg.js';
+
+export const getPosts = async (req,res) => {
+  try {
+    const postMsgs = await PostMsg.find();
+    console.log(postMsgs);
+    res.status(200).json(postMsgs);
+  } catch (error){
+    res.status(404).json({message: error.message});
+  }
 };
 
 export const createPosts = (req,res) => {
