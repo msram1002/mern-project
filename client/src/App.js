f0353c84a13bda8dad6a59490296dab8c3921a80
Sundara Ram Matta <msram0210@gmail.com>
2021-10-03 21:57:49 -0400

    importing the styles and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.png';
+import MemoriesPosts from './components/MemoriesPosts/MemoriesPosts';
+import MemoriesForm from './components/MemoriesForm/MemoriesForm';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h3" align="center">
+      <AppBar className={
+      classes.appBar} position="static" color="inherit">
+        <Typography className={
+      classes.heading} variant="h3" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img src={memories} alt="memories" className={
+      classes.img} height="60" />
       </AppBar>
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              
+              <MemoriesPosts />
             </Grid>
             <Grid item xs={12} sm={7}>
-              
+              <MemoriesForm />
             </Grid>
           </Grid>
         </Container>
